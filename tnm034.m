b7bdd26264abcd5c23919fd6203b49c1949f26b8
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2015-11-05 09:10:20 +0100

    Added main function.

---
+++ tnm034.m
@@ -0,0 +1,12 @@
+function [ output_args ] = tnm034( im )
+%
+% im: Image of unknown face, RGB-image in uint8 format in the
+% range [0,255]
+%
+% id: The identity number (integer) of the identified person,
+% i.e. ‘1’, ‘2’,…,‘16’ for the persons
+
+% I'm done. Fix it all, boys!
+
+end
+
