bb55d21fbd62a099346e13cdc9c8026483ce7ac2
unknown <pelse862@student.liu.se>
2015-11-10 11:37:22 +0100

    Added a mouth detection in testet.m

--- testet.m
+++ testet.m
@@ -1,4 +1,4 @@
-im = imread('images/DB0/db0_1.jpg');
+im = imread('images/DB0/db0_4.jpg');
 
 im2 = whiteBalance(im);
 
@@ -8,17 +8,50 @@ im2Y = im2double(im2(:,:,1));
 im2Cb = im2double(im2(:,:,2));
 im2Cr = im2double(im2(:,:,3));
 
-imshow(im2Cb./im2Y);
+%{
+imshow(im2Cr./im2Cb);
 figure;
-imshow(im2Cr./im2Y);
+imshow(pow2(im2Cr));
+figure
+imshow(im2Cr./im2Cb - pow2(im2Cr));
 
-faceMask = im2Cr./im2Y - im2Cb./im2Y > 0.1;
+figure
+imshow((im2Cr./im2Cb - pow2(im2Cr)).*pow2(im2Cr));
 
-se = strel('disk', 3);
-se2 = strel('disk', 9);
-se3 = strel('disk', 6);
+%}
 
-dilateFace = imerode(imdilate(imerode(faceMask, se), se2), se3);
+
+[c, r] = size(im2Cr);
+chromaLength = c*r;
+
+top = ((1/chromaLength) * sum(sum( ( im2Cr(:,:).*im2Cr) )));
+bot = ((1/chromaLength)* sum(sum(im2Cr(:,:)./im2Cb(:,:))));
+n = 0.95 *( top./bot );
+
+faceMask = im2Cr./im2Y - im2Cb./im2Y > 0.001;
+
+mouthMask = ((im2Cr.*im2Cr) .* (((im2Cr.*im2Cr) - n.*(im2Cr./im2Cb)).^2))>0.001;
+
+
+
+figure
+imshow(mouthMask);
+
+    
+
+
+%faceMask = im2Cr./im2Y - im2Cb./im2Y > 0.1;
+se = strel('disk', 1);
+se2 = strel('disk', 4);
+se3 = strel('disk', 3);
+
+%dilateFace = imdilate(mouthMask ,se2);
+
+dilateFace = imerode(imdilate(imerode(mouthMask, se),se2),se3);
+
+figure
+imshow(dilateFace);
+
 
 im2 = im2double(im);
 im2r = im2(:,:,1);
