8463d13cb2279b2d29bf868328ceb28674e7291e
Torsten92 <torstengustavsson1992@gmail.com>
2015-11-12 10:49:59 +0100

    mouthmask under construction

--- testet.m
+++ testet.m
@@ -8,50 +8,18 @@ im2Y = im2double(im2(:,:,1));
 im2Cb = im2double(im2(:,:,2));
 im2Cr = im2double(im2(:,:,3));
 
-%{
-imshow(im2Cr./im2Cb);
+faceMask = im2Cr./im2Y - im2Cb./im2Y > 0.10;
+
+faceMask = imfill(faceMask, 'holes');
 figure;
-imshow(pow2(im2Cr));
-figure
-imshow(im2Cr./im2Cb - pow2(im2Cr));
+imshow(faceMask);
 
-figure
-imshow((im2Cr./im2Cb - pow2(im2Cr)).*pow2(im2Cr));
+se = strel('disk', 3);
+se2 = strel('disk', 9);
+se3 = strel('disk', 6);
 
-%}
-
-
-[c, r] = size(im2Cr);
-chromaLength = c*r;
-
-top = ((1/chromaLength) * sum(sum( ( im2Cr(:,:).*im2Cr) )));
-bot = ((1/chromaLength)* sum(sum(im2Cr(:,:)./im2Cb(:,:))));
-n = 0.95 *( top./bot );
-
-faceMask = im2Cr./im2Y - im2Cb./im2Y > 0.001;
-
-mouthMask = ((im2Cr.*im2Cr) .* (((im2Cr.*im2Cr) - n.*(im2Cr./im2Cb)).^2))>0.001;
-
-
-
-figure
-imshow(mouthMask);
-
-    
-
-
-%faceMask = im2Cr./im2Y - im2Cb./im2Y > 0.1;
-se = strel('disk', 1);
-se2 = strel('disk', 4);
-se3 = strel('disk', 3);
-
-%dilateFace = imdilate(mouthMask ,se2);
-
-dilateFace = imerode(imdilate(imerode(mouthMask, se),se2),se3);
-
-figure
-imshow(dilateFace);
-
+dilateFace = imerode(imdilate(imerode(faceMask, se), se2), se3);
+dilateFace = imfill(dilateFace, 'holes');
 
 im2 = im2double(im);
 im2r = im2(:,:,1);
@@ -64,3 +32,91 @@ im2 = cat(3, im2r, im2g, im2b);
 
 figure;
 imshow(im2);
+
+horizontalProfile = mean(dilateFace, 1) > 0.01; % Or whatever.
+firstColumn = find(horizontalProfile, 1, 'first');
+lastColumn = find(horizontalProfile, 1, 'last');
+verticalProfile = mean(dilateFace, 2) > 0.01; % Or whatever.
+firstRow = find(verticalProfile, 1, 'first');
+lastRow = find(verticalProfile, 1, 'last');
+subImage = im(firstRow:lastRow, firstColumn:lastColumn,:);
+%%
+
+%MouthDetection
+
+subImage = rgb2ycbcr(subImage);
+
+im2Y = im2double(subImage(:,:,1));
+im2Cb = im2double(subImage(:,:,2));
+im2Cr = im2double(subImage(:,:,3));
+
+figure
+imshow(im2Cr);
+figure
+imshow(im2Cb);
+
+%calculate n, "eta"
+chromaLength = size(im2Cr(:), 1);
+top = ((1/chromaLength) * sum(im2Cr(:).*im2Cr(:)));
+bot = ((1/chromaLength) * sum(im2Cr(:)./im2Cb(:)));
+n = 0.95 *( top./bot );
+
+%calculate mouthmask 
+mouthMask = ((im2Cr.*im2Cr) .* (((im2Cr.*im2Cr) - n.*(im2Cr./im2Cb)).^2));
+
+%show mask and the "cleaned" image
+figure
+imshow(mouthMask./(max(mouthMask(:))));
+%%
+se = strel('disk', 1);
+se2 = strel('disk', 1);
+se3 = strel('disk', 1);
+dilateFace = imerode(imdilate(imerode(mouthMask, se),se2),se3);
+
+figure
+imshow(dilateFace);
+
+
+%{
+%Eye Detection
+%%
+%Chrominance eyeMap
+
+Cb2 = im2Cb.*im2Cb;
+Cr2 = (1-im2Cr).*(1-im2Cr);
+CbCr = im2Cb./im2Cr;
+eyeMapC = (1/3) .* (Cb2 +Cr2+CbCr);
+
+%Luminance eyeMap
+se4 = strel('disk', 1);
+eyeMapL = imdilate(im2Y, se4)./(imerode(im2Y,se4)+1);
+
+
+imshow(eyeMapC);
+figure
+imshow(eyeMapL);
+figure
+
+
+%full eyeMap
+eyeMap = eyeMapC.*eyeMapL;
+%imshow(eyeMap);
+se5 = strel('disk', 1);
+dilatedEyeMap = imdilate(eyeMap, se5);
+imshow(dilatedEyeMap);
+
+
+%%
+
+im2 = im2double(im);
+im2r = im2(:,:,1);
+im2g = im2(:,:,2);
+im2b = im2(:,:,3);
+im2r(im2r > dilateFace) = 0;
+im2g(im2g > dilateFace) = 0;
+im2b(im2b > dilateFace) = 0;
+im2 = cat(3, im2r, im2g, im2b);
+
+figure;
+imshow(im2);
+%}
