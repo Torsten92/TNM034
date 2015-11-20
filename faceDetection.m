e5554b2f3b151ddf9f598cde2ab8717c248fb0b5
unknown <pelse862@student.liu.se>
2015-11-20 11:14:51 +0100

    Added some functionality

--- faceDetection.m
+++ faceDetection.m
@@ -1,19 +1,19 @@
 %function [massa argument] = faceDetection(image)
-for i = 1:9
-    imageName = sprintf('images/DB1/db1_0%d.jpg',i)
-    image = imread(imageName);
+%for i = 1:9
+  %imageName = sprintf('images/DB1/db1_0%d.jpg',i)
+  %image = imread(imageName);
+  image = imread('images/DB1/db1_05.jpg');
 
     image = whiteBalance(image);
 
-
     [~, subImage, subFaceMask] = skinDetection(image);
 
-
     [~, mouthImg] = mouthDetection(subImage);
-
+    
     [~, eyeImg] = eyeDetection(subImage, subFaceMask);
+    figure;imshow(eyeImg)
 
     [~, triImg] = triangulateFace(mouthImg, eyeImg, subImage);
 
     figure;imshow(triImg)
-end
+%end
--- triangulateFace.m
+++ triangulateFace.m
@@ -12,41 +12,63 @@ ycentre = round(ycentre);
 
 %subplot into 2 images
 [c,r] = imfindcircles(eyeImg,[10,20]);
+viscircles(c, r);
+%figure;imshow(eyeImg);
 
-x1 = 0;
-y1 = 0;
-x2 = 0;
-y2 = 0;
+x1 = 1;
+y1 = 1;
+x2 = 2;
+y2 = 2;
 
 [row ~] = size(c);
 c = round(c);
-if(r>2)
-    d = (1:row);
-    eyesCenter=[xcentre ycentre;
-                0 0];
-    for n = 1:row
-        eyesCenter(2,1) = c(n,1);
-        eyesCenter(2,2) = c(n,2);
-        
-        d(n) = pdist(eyesCenter,'minkowski');
-
+boolFlag = 0;
+%check if all eyes are below mouth
+for n = 1:row
+    if (c(n,2) > ycentre)
+        boolFlag = boolFlag+1;
     end
-    l = find(d == min(d));
-    x1 = c(l,1);
-    y1 = c(l,2);
-    d(d == min(d)) = 1000000;
-    l2 = find(d == min(d));
-    x2 = c(l2,1);
-    y2 = c(l2,2);
-    
-else
-    x1 = c(1,1);
-    y1 = c(2,1);
-    x2 = c(2,1);
-    y2 = c(2,2);
 end
+if(boolFlag ~= row)
+    %if there are more than 2 eyes
+    if(r>2)
+        d = (1:row);
+        eyesCenter=[xcentre ycentre;
+                    1 1];
+        %check distance to each eye
+        for n = 1:row
+            eyesCenter(2,1) = c(n,1);
+            eyesCenter(2,2) = c(n,2);
+            lengthToPoint = pdist(eyesCenter,'minkowski');
+            %if eye are below mouth set as a high index
+            if(lengthToPoint > 30 && c(n,2) < ycentre)
+               d(n) = lengthToPoint;
+            else
+               d(n) = 1000000;
+            end
+        
+        end
+        %check smallest distance
+        l = find(d == min(d));
+        x1 = c(l,1);
+        y1 = c(l,2);
+        d(d == min(d)) = 1000000;
+        l2 = find(d == min(d));
+        x2 = c(l2,1);
+        y2 = c(l2,2);
 
-polygon = int32([x1 y1 x2 y2 xcentre ycentre]); 
-shapeInserter = vision.ShapeInserter('Shape','Polygons','BorderColor','Custom', 'CustomBorderColor', uint8([255 0 0]));
+    else
+        %if there are only 2 eyes
+        x1 = c(1,1);
+        y1 = c(2,1);
+        x2 = c(2,1);
+        y2 = c(2,2);
+    end
+end
+    polygon = int32([x1 y1 x2 y2 xcentre ycentre]); 
+    shapeInserter = vision.ShapeInserter('Shape','Polygons','BorderColor','Custom', 'CustomBorderColor', uint8([255 0 0]));
 
-triImg = step(shapeInserter, subImage, polygon); 
+    triImg = step(shapeInserter, subImage, polygon); 
+
+
+
