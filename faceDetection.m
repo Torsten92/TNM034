f3ab0b9f2e7483e810ac6dbb7b5b0acf274ffe60
unknown <pelse862@student.liu.se>
2015-11-26 09:52:32 +0100

    eyeMap

--- faceDetection.m
+++ faceDetection.m
@@ -1,16 +1,22 @@
 %function [massa argument] = faceDetection(image)
 clc;
-%for i = 1:9
-    image = imread(sprintf('images/DB1/db1_0%d.jpg', 9));
+clear all
+for i = 1:1
+    image = imread(sprintf('images/DB1/db1_0%d.jpg', i));
     %image = imread(sprintf('images/DB0/db0_%d.jpg',2));
     image = whiteBalance(image);
 
     [~, subImage, subFaceMask] = skinDetection(image);
     [~, mouthImg, mouthCenter] = mouthDetection(subImage);
     [xPos, yPos ,~, eyeImg] = eyeDetection(subImage, subFaceMask, mouthCenter);
+  
+      
+    figure;imshow(subImage)
+
+
     [~, triImg] = triangulateFace(xPos,yPos,subImage,mouthCenter);
     
-    corrVal = compareToDB(subImage, subFaceMask, mouthImg, eyeImg, triImg);
+    %corrVal = compareToDB(subImage, subFaceMask, mouthImg, eyeImg, triImg);
     %figure;imshow(mouthImg)
     %imshow(subFaceMask)
-%end
+end
