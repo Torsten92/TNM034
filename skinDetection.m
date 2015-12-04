7bb81d52f8d3bea61f5f5ada02096c3612bedb53
Erik <eriol726@student.liu.se>
2015-12-04 11:32:34 +0100

    fixed min faceArea

--- skinDetection.m
+++ skinDetection.m
@@ -1,181 +1,111 @@
-function [cropImage, faceMask] = skinDetection(image,sumSize)
-
-
-cbcrIm = rgb2ycbcr(image);
+function [img, faceMask] = skinDetection(image)
 
 [~, skinRegion] = generate_skinmap(image);
-
-
-Y = double(cbcrIm(:,:,1));
-Cb = double(cbcrIm(:,:,2));
-Cr = double(cbcrIm(:,:,3));
-
-
-%find white parts = red dots
-[rowSkinColor, colSkinColor] = find(skinRegion == 1);
-
-%find black parts = blue dots
-[rowRepColor, colRepColor] = find(skinRegion == 0);
-
-
-[skinColLenth, ~] = size(rowSkinColor);
-[repColLenth, ~] = size(rowRepColor);
-
-skinColorY = zeros(skinColLenth,1);
-skinColorCb = zeros(skinColLenth,1);
-skinColorCr = zeros(skinColLenth,1);
-
-repColorY = zeros(repColLenth,1);
-repColorCb = zeros(repColLenth,1);
-repColorCr = zeros(repColLenth,1);
-
-
-%find skin color value in lightComp image with skin color positions
-for i = 1:skinColLenth
-        skinColorY(i,1) = Y(rowSkinColor(i),colSkinColor(i));
-        skinColorCr(i,1) = Cr(rowSkinColor(i),colSkinColor(i));
-        skinColorCb(i,1) = Cb(rowSkinColor(i),colSkinColor(i));
-end
-
-for i = 1:repColLenth
-        repColorCb(i,1) = Cb(rowRepColor(i),colRepColor(i));
-        repColorCr(i,1) = Cr(rowRepColor(i),colRepColor(i));
-        repColorY(i,1) = Y(rowRepColor(i),colRepColor(i));
-end
-
 
 %fill holes
 groupedSkinArea = imfill(skinRegion, 'holes');
 
-
-
-se = strel('disk', 3);
-se2 = strel('disk', 9);
-se3 = strel('disk', 6);
-
 %remove noise
-faceMask = imerode(imdilate(imerode(groupedSkinArea, se), se2), se3);
+se = strel('disk', 9);
+faceMask = (imdilate(groupedSkinArea, se));
 faceMask = imfill(faceMask, 'holes');
 
 %decide how many pixels a region must have to not be erased 
 [r, c] = size(faceMask);
 numbOfpixels = round(r*c*0.033);
+
 %erase white regions if it contains less than numbOfpixels pixels, we only
 %want one face region
 faceMask = bwareaopen(faceMask, numbOfpixels);
 
-%expand the face to an elipse mask
-%faceMask = ellipse_mask+faceMask;
-
-%faceMask=  faceMask > 0.1;
-
-%noze reduction
+%noise reduction
 se = strel('disk', 20);
 faceMask = imfill(faceMask, 'holes');
 faceMask = imdilate(imerode(faceMask, se), se);
-
-%invert color
-%ellipse_mask = imcomplement(ellipse_mask);
-
-
-[row, col] = find(faceMask);
-    cropImage  = image(min(row):max(row), min(col):max(col), :);
-    faceMask  = faceMask(min(row):max(row), min(col):max(col));
-    %cropImage = imcrop(image,[centroidsOffaceMask(n).BoundingBox]);
-
-
-
-
-
 %masking, gives the complete mask
 
-centroidsOffaceMask  = regionprops(skinRegion,'BoundingBox','Area');
+
+%find all areas of white 
+centroidsOffaceMask  = regionprops(faceMask,'BoundingBox','Area');
+
+assignin('base', 'centroidsOffaceMask', centroidsOffaceMask);
 
 
-MaxArea = numbOfpixels; %// Select largest area you want to keep.
-centroidsOffaceMask = centroidsOffaceMask([centroidsOffaceMask.Area] > MaxArea); %// Detect cells larger than some value.
+% Decide min area for faces to be detected
+[r c] = size(image);
+MinArea = 0.02*r*c; 
+L = length(centroidsOffaceMask);
+assignin('base', 'L', L);
+%[row, col] = find(faceMask);
+   % cropSubImage  = image(min(row):max(row), min(col):max(col), :);
 
-
-L = length(centroidsOffaceMask)
 
 for n = 1:L
-    %ta ut alla omr�den i masken
-    
+    if centroidsOffaceMask(n).Area > MinArea
+        %croppar ut alla omr�den i bilden till sub-bilder
+        cropSubImage = imcrop(image,[centroidsOffaceMask(n).BoundingBox]);
 
-    %cropImage = imcrop(image,[centroidsOffaceMask(n).BoundingBox]);
+        %generera om skinmasken p� omr�det
+        [~, skinRegion] = generate_skinmap(cropSubImage);
 
-    %figure;imshow(cropImage);
-    %generera om skinmasken p� omr�det
+        %fill holes
+        groupedSkinArea = imfill(skinRegion, 'holes');
 
-    [~, skinRegion] = generate_skinmap(cropImage);
-    
-    
-    
-    %fill holes
-    groupedSkinArea = imfill(skinRegion, 'holes');
+        se = strel('disk', 3);
+        se2 = strel('disk', 9);
+        se3 = strel('disk', 6);
 
-    se = strel('disk', 3);
-    se2 = strel('disk', 9);
-    se3 = strel('disk', 6);
+        %remove noise
+        faceMask = imerode(imdilate(imerode(groupedSkinArea, se), se2), se3);
+        faceMask = imfill(faceMask, 'holes');
+        %decide how many pixels a region must have to not be erased 
+        [r, c] = size(faceMask);
+        numbOfpixels = round(r*c*0.043);
+        %erase white regions if it contains less than numbOfpixels pixels, we only
+        %want one face region
+        faceMask = bwareaopen(faceMask, numbOfpixels);
 
-    %remove noise
-    faceMask = imerode(imdilate(imerode(groupedSkinArea, se), se2), se3);
-    faceMask = imfill(faceMask, 'holes');
-    %decide how many pixels a region must have to not be erased 
-    [r, c] = size(faceMask);
-    numbOfpixels = round(r*c*0.043);
-    %erase white regions if it contains less than numbOfpixels pixels, we only
-    %want one face region
-    faceMask = bwareaopen(faceMask, numbOfpixels);
-   
-    %find all "ones" in faceMask
-    [x, y] = find(faceMask);
+        %find all "ones" in faceMask
+        [x, y] = find(faceMask);
 
-    X = [x';
-        y'];
+        X = [x';
+            y'];
 
-    %ellipse mask
-    [zt, at, bt, ~] = fitellipse(X, 'linear', 'constraint', 'trace');
-    ellipseC = zt;
-    r_sq = [bt, at].^2;
+        %ellipse mask
 
-    [sizeX, sizeY] = size(faceMask);
-    [X, Y] = meshgrid(1:sizeY, 1:sizeX);
-    ellipse_mask = ((r_sq(1) * (Y - ellipseC(1)) .^ 2 + r_sq(2) * (X - ellipseC(2)) .^ 2) <= prod(r_sq));
+        [zt, at, bt, ~] = fitellipse(X, 'linear', 'constraint', 'trace');
+        ellipseC = zt;
+        r_sq = [bt, at].^2;
 
-    %makes the elips bigger, important because somtimes it cuts eyes and mouths
-    se = strel('disk', 20);
-    ellipse_mask = imdilate(ellipse_mask,se);
-    
-     
-    faceMaskPlusElips = faceMask+ellipse_mask;
-    faceMaskPlusElips(faceMaskPlusElips > 0.1);
-    faceMask = faceMaskPlusElips > 0.1;
-    
-    assignin('base', 'faceMask', faceMask);
-    assignin('base', 'cropImage', cropImage);
-    cropImage = im2double(cropImage);
-    img(:,:,1) = cropImage(:,:,1).*faceMask;
-    img(:,:,2) = cropImage(:,:,2).*faceMask;
-    img(:,:,3) = cropImage(:,:,3).*faceMask;
-    figure;imshow(img);
+        [sizeX, sizeY] = size(faceMask);
+        [X, Y] = meshgrid(1:sizeY, 1:sizeX);
+        ellipse_mask = ((r_sq(1) * (Y - ellipseC(1)) .^ 2 + r_sq(2) * (X - ellipseC(2)) .^ 2) <= prod(r_sq));
 
-    [~, mouthImg, mouthCenter] = mouthDetection(cropImage, faceMask);
-    
-    if ( isnan(mouthCenter(1))==0 && isnan(mouthCenter(2)) == 0  )
+        %makes the ellipse bigger, important because somtimes it cuts eyes and mouths
+        se = strel('disk', 20);
+        ellipse_mask = imdilate(ellipse_mask,se);
 
-        [xPos, yPos, corrVal, eyeImg] = eyeDetection(img, faceMask, mouthCenter,sumSize);
-    
+        faceMaskPlusElips = ellipse_mask;
+        faceMaskPlusElips = (faceMaskPlusElips > 0.1);
+        faceMask = faceMaskPlusElips > 0.1;
 
-        [~, triImg] = triangulateFace(xPos,yPos,cropImage,mouthCenter);
-         
-        figure;imshow(triImg);
+        assignin('base', 'faceMask', faceMask);
+        assignin('base', 'cropSubImage', cropSubImage);
+        cropSubImage = im2double(cropSubImage);
+        img = zeros(size(cropSubImage));
+        img(:,:,1) = cropSubImage(:,:,1).*faceMask;
+        img(:,:,2) = cropSubImage(:,:,2).*faceMask;
+        img(:,:,3) = cropSubImage(:,:,3).*faceMask;
+
+        %Mouth detection
+        [~, ~, mouthCenter] = mouthDetection(cropSubImage, faceMask);
+
+        %Detect eyes and rotate image to align them to the horizontal plane
+        [xPos, yPos, ~] = eyeDetection(img, faceMask, mouthCenter);
+        [angle, ~] = triangulateFace(xPos,yPos,img,mouthCenter);
+        img = imrotate(img, angle, 'bilinear');
     else
-        disp('ingen mun hittades')
-        %om det inte finns en mun SKA allt explodera 
-        
+        disp('too smal face area')
+        n = n + 1;
     end
-    
-    
 end
-
