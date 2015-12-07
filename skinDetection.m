eb3f2a86e929f35f2331c8c7efbef27d5400d40c
Erik <eriol726@student.liu.se>
2015-12-07 14:06:24 +0100

    added commet to ellipse funtion

--- skinDetection.m
+++ skinDetection.m
@@ -1,5 +1,7 @@
 function [img, faceMask] = skinDetection(image)
 
+img = 0;
+
 [~, skinRegion] = generate_skinmap(image);
 
 %fill holes
@@ -28,14 +30,11 @@ faceMask = imdilate(imerode(faceMask, se), se);
 %find all areas of white 
 centroidsOffaceMask  = regionprops(faceMask,'BoundingBox','Area');
 
-assignin('base', 'centroidsOffaceMask', centroidsOffaceMask);
-
 
 % Decide min area for faces to be detected
 [r c] = size(image);
 MinArea = 0.02*r*c; 
 L = length(centroidsOffaceMask);
-assignin('base', 'L', L);
 %[row, col] = find(faceMask);
    % cropSubImage  = image(min(row):max(row), min(col):max(col), :);
 
@@ -59,7 +58,7 @@ for n = 1:L
         faceMask = imerode(imdilate(imerode(groupedSkinArea, se), se2), se3);
         faceMask = imfill(faceMask, 'holes');
         %decide how many pixels a region must have to not be erased 
-        [r, c] = size(faceMask);
+        [row, col] = size(faceMask);
         numbOfpixels = round(r*c*0.043);
         %erase white regions if it contains less than numbOfpixels pixels, we only
         %want one face region
@@ -67,42 +66,78 @@ for n = 1:L
 
         %find all "ones" in faceMask
         [x, y] = find(faceMask);
+        X = [x'; y'];
+        
+        %calculate coefficient a and b and the center point
+        [z, a, b, ~] = fitellipse(X, 'linear');
+        
+        %the ellipses center coords
+        h = z(1);
+        k = z(2);
 
-        X = [x';
-            y'];
+        %meshgrid uses x and y coords, we want rows represent y and x
+        %represent cols
+        [Y, X] = meshgrid(1:col, 1:row);
 
-        %ellipse mask
-
-        [zt, at, bt, ~] = fitellipse(X, 'linear', 'constraint', 'trace');
-        ellipseC = zt;
-        r_sq = [bt, at].^2;
-
-        [sizeX, sizeY] = size(faceMask);
-        [X, Y] = meshgrid(1:sizeY, 1:sizeX);
-        ellipse_mask = ((r_sq(1) * (Y - ellipseC(1)) .^ 2 + r_sq(2) * (X - ellipseC(2)) .^ 2) <= prod(r_sq));
+        % An ellipse centered at (h,k) is defined by (x-h)^2/a^2 + (y-k)^2/b^2 = 1 
+        a2 = a^2;
+        b2 = b^2;
+        a2b2 = a2*b2;
+        
+        ellipse_mask = (b2*(X-h).^2 + a2*(Y-k).^2 <= a2b2);
 
         %makes the ellipse bigger, important because somtimes it cuts eyes and mouths
         se = strel('disk', 20);
         ellipse_mask = imdilate(ellipse_mask,se);
-
+        
         faceMaskPlusElips = ellipse_mask;
         faceMaskPlusElips = (faceMaskPlusElips > 0.1);
         faceMask = faceMaskPlusElips > 0.1;
 
-        assignin('base', 'faceMask', faceMask);
-        assignin('base', 'cropSubImage', cropSubImage);
         cropSubImage = im2double(cropSubImage);
         img = zeros(size(cropSubImage));
-        img(:,:,1) = cropSubImage(:,:,1).*faceMask;
-        img(:,:,2) = cropSubImage(:,:,2).*faceMask;
-        img(:,:,3) = cropSubImage(:,:,3).*faceMask;
+        %img(:,:,1) = cropSubImage(:,:,1).*faceMask;
+        %img(:,:,2) = cropSubImage(:,:,2).*faceMask;
+        %img(:,:,3) = cropSubImage(:,:,3).*faceMask;
+        img = cropSubImage;
 
         %Mouth detection
         [~, ~, mouthCenter] = mouthDetection(cropSubImage, faceMask);
+        if isnan(mouthCenter(1)) == 0
+            %Detect eyes and rotate image to align them to the horizontal plane
+            [leftEye, rightEye, ~] = eyeDetection(img, faceMask, mouthCenter);
+            [angle, ~] = triangulateFace(leftEye,rightEye,img,mouthCenter);
 
-        %Detect eyes and rotate image to align them to the horizontal plane
-        [xPos, yPos, ~] = eyeDetection(img, faceMask, mouthCenter);
-        [angle, ~] = triangulateFace(xPos,yPos,img,mouthCenter);
-        img = imrotate(img, angle, 'bilinear');
+            %rotate all
+            img = imrotate(img, angle, 'bilinear');
+            faceMask = imrotate(faceMask, angle, 'bilinear');
+
+            %redo all calculations for the rotated image
+            %Mouth detection
+            [~, ~, mouthCenter] = mouthDetection(img, faceMask);
+
+            %Detect eyes and rotate image to align them to the horizontal plane
+            [leftEye, rightEye, ~] = eyeDetection(img, faceMask, mouthCenter);    
+
+
+            xSize = round(0.2*abs(rightEye(1,1)-leftEye(1,1)));
+            ySize = round(0.2*abs(rightEye(1,2)-mouthCenter(1,2)));
+
+            %calculate new mask
+            c = [leftEye(1,1)-xSize rightEye(1,1)+xSize rightEye(1,1)+xSize leftEye(1,1)-xSize];
+            r = [leftEye(1,2)-ySize rightEye(1,2)-ySize mouthCenter(1,2)+ySize mouthCenter(1,2)+ySize];
+            faceMask2 = roipoly(faceMask,c,r);
+
+            %recalculate and crop image with new mask
+            img(:,:,1) = img(:,:,1).*faceMask2;
+            img(:,:,2) = img(:,:,2).*faceMask2;
+            img(:,:,3) = img(:,:,3).*faceMask2;
+            [row, col] = find(faceMask2);
+            %faceMask2  = faceMask2(min(row):max(row), min(col):max(col));
+            img = img(min(row):max(row), min(col):max(col),:);
+
+            %Normalize illumination
+            img = 0.4 + (log(img)+0.4) ./ 3;
+        end 
     end
 end
