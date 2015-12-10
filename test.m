87f3648f02dfc7202ca408f82cd2769475e600c9
Erik <eriol726@student.liu.se>
2015-12-10 14:55:37 +0100

    change git ignore

--- test.m
+++
@@ -1,8 +0,0 @@
-image = imread('images/db1_07.jpg');
-image =rgb2ycbcr(image);
-A =  reshape(image(:,:,1),[],1);
-A = double(A);
-
-img = 0.5 + (log(A)+1) ./ (5*log(35));
-img = sort(img);
-plot(img)
